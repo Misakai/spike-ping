e62ce531e54e7b0d7d6994a99699494d2f05d6a6
Roman <roman.atachiants@gmail.com>
2015-03-18 01:10:27 +0000

    samples and print fix

--- src/sping.go
+++ src/sping.go
@@ -10,18 +10,16 @@ import(
 	"github.com/montanaflynn/stats"
 )
 
-var t0 time.Time
 var samples []float64
 
 func main() {
-	t0  = time.Now()
 	app := cli.NewApp()
 	app.Name = "sping"
 	app.Usage = "Pinging and latency measurement utility for Spike Engine 3 powered services."
 	app.Flags = []cli.Flag {
 		cli.StringFlag {
 			Name: "interval",
-			Value: "500ms",
+			Value: "250ms",
 			Usage: "The interval between two pings. Accepts a string as a sequence of decimal numbers, each with optional fraction and a unit suffix, such as '300ms', '-1.5h' or '2h45m'. Valid time units are 'ns', 'us' (or 'µs'), 'ms', 's', 'm', 'h'.",
 		},
 	}
@@ -32,6 +30,7 @@ func main() {
 		}
 
 		// The interval
+		t0 := time.Now()
 		interval, err := time.ParseDuration(c.String("interval"))
 		if err != nil {
 			panic(err)
@@ -43,14 +42,20 @@ func main() {
 		channel.Connect(host, 8196)
 
 		// Handle pong
-		go onPing(channel)
+		go func (){
+			for{
+		    	msg := <- channel.OnPing
+		    	rtt := int32(time.Now().Sub(t0) / 1000000) - msg.Time
+		    	fmt.Println("Pinging", host, "with 12 bytes of data:", rtt, "ms.")
+		    	samples = append(samples, float64(rtt))
+			}
+		}()
 
 		// Start pinging
 
 		for {
 			// Get the ping start
 			now := int32(time.Now().Sub(t0).Nanoseconds() / 1000000)
-			fmt.Print("Pinging ", host, " with 12 bytes of data: ")
 			channel.Ping(now)
 			time.Sleep(interval)
 		}
@@ -81,6 +86,7 @@ func main() {
 
     		fmt.Println()
     		fmt.Println("Ping statistics:")
+    		fmt.Println("   Samples:  ", len(samples), "events")
     		fmt.Println("   Min:      ", min, "ms.")
     		fmt.Println("   Max:      ", max, "ms.")
     		fmt.Println("   Mean:     ", mean, "ms.")
@@ -98,13 +104,4 @@ func main() {
 
 	// Run the application
 	app.RunAndExitOnError()
-}
-
-func onPing(channel *spike.TcpChannel){
-	for{
-    	msg := <- channel.OnPing
-    	rtt := int32(time.Now().Sub(t0) / 1000000) - msg.Time
-    	fmt.Println(rtt, " ms.")
-    	samples = append(samples, float64(rtt))
-	}
 }
